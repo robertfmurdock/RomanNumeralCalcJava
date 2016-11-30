79b9be25d1700cfb5b1ea43aefa4065717b99acb
Rob Murdock <rmurdock@pillartechnology.com>
2016-11-30 15:16:30 -0500

    Did some refactoring in the converter. Still some problems which need to be highlighted in tests.

--- src/main/java/RomanNumeralConverter.java
+++ src/main/java/RomanNumeralConverter.java
@@ -1,35 +1,67 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
+import java.util.List;
 
 class RomanNumeralConverter {
 
-    private final Map<Character, Integer> multipleOfTenMap;
+    private class NumeralValueTuple {
+
+        private final Character numeral;
+        private final int value;
+
+        NumeralValueTuple(final Character numeral, final int value) {
+            this.numeral = numeral;
+            this.value = value;
+        }
+
+        int getValue() {
+            return value;
+        }
+
+        Character getNumeral() {
+            return numeral;
+        }
+    }
+
+    private final List<NumeralValueTuple> multiplesOfTen;
 
     RomanNumeralConverter() {
-        multipleOfTenMap = new HashMap<>();
-        multipleOfTenMap.put('C', 100);
-        multipleOfTenMap.put('X', 10);
-        multipleOfTenMap.put('I', 1);
+        multiplesOfTen = Arrays.asList(
+                new NumeralValueTuple('C', 100),
+                new NumeralValueTuple('X', 10),
+                new NumeralValueTuple('I', 1)
+        );
     }
 
     int toInteger(final String numeral) {
-        return numeral.length() * multipleOfTenMap.get(numeral.charAt(0));
+        return numeral.length() * getValueFor(numeral.charAt(0));
+    }
+
+    private Integer getValueFor(final char numeral) {
+        return multiplesOfTen.stream()
+                .filter(tuple -> tuple.getNumeral().equals(numeral))
+                .findFirst()
+                .get()
+                .getValue();
     }
 
     String toNumeral(final int value) {
+        return reduceValueUntilRenderedAsNumeral(value);
+    }
+
+    private String reduceValueUntilRenderedAsNumeral(int remainingValue) {
         final StringBuilder builder = new StringBuilder();
 
-        int mutableValue = value;
-
-        for (final Map.Entry<Character, Integer> entry : multipleOfTenMap.entrySet()) {
-            final Character numeral = entry.getKey();
-            final Integer numeralValue = entry.getValue();
-
-            final int quotient = mutableValue / numeralValue;
-            repeatValue(builder, numeral, quotient);
-            mutableValue -= numeralValue * quotient;
+        for (final NumeralValueTuple numeralTuple : multiplesOfTen) {
+            final int valueConsumed = applyNumeralToString(builder, remainingValue, numeralTuple);
+            remainingValue -= valueConsumed;
         }
         return builder.toString();
+    }
+
+    private int applyNumeralToString(final StringBuilder builder, final int mutableValue, final NumeralValueTuple numeralTuple) {
+        final int quotient = mutableValue / numeralTuple.getValue();
+        repeatValue(builder, numeralTuple.getNumeral(), quotient);
+        return numeralTuple.getValue() * quotient;
     }
 
     private void repeatValue(final StringBuilder builder, final Character numeral, final int quotient) {
