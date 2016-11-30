349dfb759df00c287cf4f0332bff86258ff42f2e
Rob Murdock <rmurdock@pillartechnology.com>
2016-11-30 13:26:37 -0500

    Project initialization.

---
+++ src/main/java/RomanNumeralCalculator.java
@@ -0,0 +1,5 @@
+public class RomanNumeralCalculator {
+    public boolean someLibraryMethod() {
+        return true;
+    }
+}
---
+++ src/test/java/RomanNumeralCalculatorTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class RomanNumeralCalculatorTest {
+    @Test public void testSomeLibraryMethod() {
+        RomanNumeralCalculator classUnderTest = new RomanNumeralCalculator();
+        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    }
+}
